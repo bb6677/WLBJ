cb89579363d70bef54fef7aab60036a4e61c6eb7
honeyliu <758575873@qq.com>
2020-11-11 19:09:49 +0800

    收藏红心

--- src/views/Collect.vue
+++ src/views/Collect.vue
@@ -3,12 +3,12 @@
     <van-card
       v-for="item in collects"
       :key="item.id"
-      :desc="item.desc"
-      :title="item.name"
-      :thumb="item.coverImage"
+      :desc="item.movie.desc"
+      :title="item.movie.name"
+      :thumb="item.movie.coverImage"
     >
       <template #footer>
-        <van-button size="mini" @click="rm(movie)">删除</van-button>
+        <van-button size="mini" @click="rm(item.movie.id)">删除</van-button>
         <!-- <van-button size="mini">按钮</van-button> -->
       </template>
     </van-card>
@@ -28,14 +28,15 @@ export default {
   },
   async created() {
     const res = await loadDetailAPI();
-    // console.log(res);
+    console.log(res);
     this.collects = res;
   },
   methods: {
     // eslint-disable-next-line no-unused-vars
-    async rm(movie) {
-      await del("/api/v1/user/collections/ + this.id");
+    async rm(item) {
+      await del("/api/v1/user/collections/" + item);
       Toast.success("删除成功");
+      location.reload();
     },
   },
 };
